63dfa74d1c5b10e3f15ba5663f55242118235d3e
jeffersoncharlles <contato@jefferdeveloper.com>
2023-01-21 04:37:25 -0300

    chore: weekdays box layout

--- mobile/src/components/HabitDay/index.tsx
+++ mobile/src/components/HabitDay/index.tsx
@@ -1,14 +1,20 @@
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View,Dimensions } from "react-native";
 
 interface HabitDayProps{
 
 }
+const WEEK_DAYS = 7; //por dia so 7
+const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5; // espaçamento lateral (cada lado) / 5 que e o espaçamento dos quadrados
+export const DAY_MARGIN_BETWEEN = 8;
+export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5) //tamanho de cada um //pegando tamanho da tela e dividindo por quantos
 
-export const HabitDay = ({}:HabitDayProps) => {
+export const HabitDay = ({ }: HabitDayProps) => {
+
 
     return(
       <TouchableOpacity
         className="bg-zinc-900 rounded-lg border-2 border-zinc-800 m-1"
+        style={{ width: DAY_SIZE, height: DAY_SIZE }}
       />
     );
 }
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
-import { View } from "react-native"
-import { HabitDay } from "../../components/HabitDay"
+import { Text, View } from "react-native"
+import { HabitDay ,DAY_SIZE} from "../../components/HabitDay"
 import { Header } from "../../components/Header"
+
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 export const Home = () => {
   return (
     <View className="flex-1 bg-background px-8 pt-16 text-white">
       <Header />
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekday, i) => (
+          <Text
+            key={`${weekday}-${i}`}
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{width:DAY_SIZE}}
+          >
+            {weekday}
+          </Text>
+        ))}
+      </View>
       <HabitDay/>
     </View>
   )
